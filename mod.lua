ecb50d3150a9f6f2348b6a71a1af94b3e4bb7c04
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-12-08 10:55:19 -0500

    Add example Mod:getFileSelectStyle() method

--- mod.lua
+++ mod.lua
@@ -2,3 +2,14 @@ function Mod:init()
     print("Loaded "..self.info.name.."!")
     MUSIC_PITCHES.mod_menu = 0.95
 end
+
+function Mod:getFileSelectStyle()
+    if Kristal.hasAnySaves(Mod.info.id) then
+        -- Could check for a Completion Save here
+        if Kristal.loadData("file_3") then
+            return "normal"
+        end
+        return "greatdoor"
+    end
+    return "DEVICE"
+end
