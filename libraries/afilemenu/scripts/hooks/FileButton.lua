468df8a311283f7aa5fa19070f770fc907440368
PretzelPal <pretzelpals999@gmail.com>
2025-10-07 16:08:26 +0100

    Fixed broken rectangle

--- libraries/afilemenu/scripts/hooks/FileButton.lua
+++ libraries/afilemenu/scripts/hooks/FileButton.lua
@@ -16,15 +16,10 @@ function FileButton:getDrawColor()
 end
 
 function FileButton:drawCoolRectangle(x, y, w, h)
-    if Game.world.map.menustyle == "DEVICE" then
-        love.graphics.setLineWidth(1)
-        love.graphics.setLineStyle("rough")
-        -- Set the color
-        Draw.setColor(self:getDrawColor())
-        love.graphics.rectangle("line", x, y, w, h)
-    else
-        super.drawCoolRectangle(self, x, y, w, h)
-    end
+    love.graphics.setLineWidth(4)
+    love.graphics.setLineStyle("rough")
+    Draw.setColor(self:getDrawColor())
+    love.graphics.rectangle("line", x, y, w, h)
 end
 
 function FileButton:draw()
